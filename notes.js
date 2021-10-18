735e93a15a5e40509776756ca1cb17ee2d5257f8
Raghunandan Yadav <90599332+Raghu-N-Yadav@users.noreply.github.com>
2021-10-18 19:43:15 +0530

    app

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 //console.log('this is notes.js');
 
 const getNotes = () => {return 'My notes'}
@@ -9,23 +10,38 @@ const addNote = function(title, body){
     const duplicateNotes = notes.filter( function (note) {
         return note.title === title;
     })
-
-    if (duplicateNotes.lenght == 0){
+    if (duplicateNotes.length === 0){
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes);
-        console.log('new Note Added');
-    }else{
+        console.log('New Note Added');
+    } else {
         console.log('Note Title Taken');
     }
+    
 
+}
+
+const removeNote = function(title){
+    const notes = loadNotes();
+    const notesToKeep = notes.filter(function (note){
+        return note.title !== title
+    })
+
+    saveNotes(notesToKeep);
+    if (notes.length === notesToKeep.length){
+        console.log(chalk.red.inverse('No note has removed'));
+    }else{
+        console.log(chalk.green('Note has been removed'));
+    }
+    //console.log(title);
 }
 
 const saveNotes = function(notes){
     const dataJSON = JSON.stringify(notes);
-    fs.writeFileSync('notes.json',dataJSON);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
 const loadNotes = function (){
@@ -33,12 +49,13 @@ const loadNotes = function (){
         const dataBuffer = fs.readFileSync('notes.json');
         const dataJSON =  dataBuffer.toString();
         return JSON.parse(dataJSON);
-    }catch(e){
+    } catch(e) {
         return []
     }
 }
 
 module.exports = {
     getNotes : getNotes,
-    addNote : addNote
+    addNote : addNote,
+    removeNote : removeNote
 }
